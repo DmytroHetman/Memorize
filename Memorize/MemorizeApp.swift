3db509953c4dd97300b0b54c2afc41633df133d4
Hetman Dmytro <hetmandmitriy@gmail.com>
2022-08-26 18:18:23 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dmytro Hetman on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
